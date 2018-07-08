a380928ab6976aa5c14a88b177751a81e8d89824
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-07-08 18:59:08 +0200

    Use moment to format checkIn creationInstant

--- src/services/check-in.service.ts
+++ src/services/check-in.service.ts
@@ -3,11 +3,29 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map, tap } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import moment from 'moment';
 
-export interface CheckIn {
+interface CheckInData {
   id: number;
   cardCode: string;
   creationInstant: string;
+}
+
+export class CheckIn {
+  private constructor(
+    public id: number,
+    public cardCode: string,
+    public creationInstant: moment.Moment,
+  ) {
+  }
+
+  static fromData(data: CheckInData): CheckIn {
+    return new CheckIn(
+      data.id,
+      data.cardCode,
+      moment(data.creationInstant),
+    );
+  }
 }
 
 @Injectable()
@@ -21,14 +39,15 @@ export class CheckInService {
 
   createCheckIn(cardCode: string): Observable<CheckIn> {
     const createCheckInRequest = {cardCode};
-    return this.httpClient.post<CheckIn>('/api/check-ins', createCheckInRequest).pipe(
+    return this.httpClient.post<CheckInData>('/api/check-ins', createCheckInRequest).pipe(
+      map(CheckIn.fromData),
       tap(checkIn => this.checkInsSubject.next([checkIn, ...this.checkInsSubject.getValue()]))
     );
   }
 
   loadCheckIns(): Observable<CheckIn[]> {
-    return this.httpClient.get<{ checkIns: CheckIn[] }>('/api/check-ins').pipe(
-      map(response => response.checkIns),
+    return this.httpClient.get<{ checkIns: CheckInData[] }>('/api/check-ins').pipe(
+      map(response => response.checkIns.map(CheckIn.fromData)),
       tap(checkIns => this.checkInsSubject.next(checkIns))
     );
   }
