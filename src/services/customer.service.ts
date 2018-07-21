882750cb06b6a2454fb96adfc93a808b56e88fe3
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-07-22 00:42:28 +0200

    Remove unused import from CustomerService

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { map, tap } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { map } from 'rxjs/operators';
 
 interface CardData {
   id: number;
