e669ed71f4a3ac5a9a0f340d16d1ffe458d1e931
Marcin Majkowski <pl.marcin.m@gmail.com>
2018-07-25 22:12:42 +0200

    Introduce temporary solution in PaymentService since api now returns customerId instead of PaymentCustomerData

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,43 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, withLatestFrom } from 'rxjs/operators';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Customer } from '../membership/models/customer.model';
 import { ToastController } from 'ionic-angular';
 import { CurrencyPipe } from '@angular/common';
 import { Iso8601String } from '../membership/models/iso-8601-string.model';
-
-interface PaymentCustomerData {
-  id: number;
-  firstName: string;
-  lastName: string;
-}
-
-export class PaymentCustomer {
-
-  public fullName: string;
-
-  private constructor(
-    public id: number,
-    public firstName: string,
-    public lastName: string,
-  ) {
-    this.fullName = lastName.length > 0 ? `${firstName} ${lastName}` : firstName;
-  }
-
-  static fromData(data: PaymentCustomerData) {
-    return new PaymentCustomer(
-      data.id,
-      data.firstName,
-      data.lastName,
-    );
-  }
-}
+import * as fromStore from '../membership/store';
+import { Store } from '@ngrx/store';
 
 interface PaymentData {
   id: number;
-  customer: PaymentCustomerData;
+  customerId: number;
   amount: number;
   timestamp: Iso8601String;
 }
@@ -46,16 +21,16 @@ export class Payment {
 
   private constructor(
     public id: number,
-    public customer: PaymentCustomer,
+    public customer: Customer,
     public amount: number,
     public timestamp: Iso8601String,
   ) {
   }
 
-  static fromData(data: PaymentData): Payment {
+  static fromData(data: PaymentData, customer: Customer): Payment {
     return new Payment(
       data.id,
-      data.customer && PaymentCustomer.fromData(data.customer),
+      customer,
       data.amount,
       data.timestamp,
     );
@@ -74,12 +49,15 @@ export class PaymentService {
 
   constructor(private httpClient: HttpClient,
               private toastController: ToastController,
+              // FIXME this is a temporary solution until local state remains in this service instead of ngrx
+              private store: Store<fromStore.MembershipState>,
               private currencyPipe: CurrencyPipe) {
   }
 
   createPayment(customer: Customer, createPaymentForm: CreatePaymentForm): Observable<Payment> {
     return this.httpClient.post<PaymentData>(`/api/customers/${customer.id}/payments`, createPaymentForm).pipe(
-      map(Payment.fromData),
+      withLatestFrom(this.store.select(fromStore.getCustomersEntities)),
+      map(([paymentData, customerEntities]) => Payment.fromData(paymentData, customerEntities[paymentData.customerId])),
       // TODO sorting
       tap(payment => this.paymentsSubject.next([payment, ...this.paymentsSubject.getValue()])),
       tap(payment => this.presentToast(customer, payment))
@@ -88,14 +66,18 @@ export class PaymentService {
 
   loadPayments(): Observable<Payment[]> {
     return this.httpClient.get<{ payments: PaymentData[] }>('/api/payments').pipe(
-      map(response => response.payments.map(Payment.fromData)),
+      map(response => response.payments),
+      withLatestFrom(this.store.select(fromStore.getCustomersEntities)),
+      map(([paymentDatas, customerEntities]) => paymentDatas.map(data => Payment.fromData(data, customerEntities[data.customerId]))),
       tap(payments => this.paymentsSubject.next(payments))
     );
   }
 
   getCustomerPayments(customer: Customer): Observable<Payment[]> {
     return this.httpClient.get<{ payments: PaymentData[] }>(`/api/customers/${customer.id}/payments`).pipe(
-      map(response => response.payments.map(Payment.fromData)),
+      map(response => response.payments),
+      withLatestFrom(this.store.select(fromStore.getCustomersEntities)),
+      map(([paymentDatas, customerEntities]) => paymentDatas.map(data => Payment.fromData(data, customerEntities[data.customerId]))),
     );
   }
 
